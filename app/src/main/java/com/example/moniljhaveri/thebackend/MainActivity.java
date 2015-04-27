d100856fc7fcbe13d21463ac8ef1b308ae154a29
evanbowman <ebowman@bu.edu>
2015-04-27 18:32:16 -0400

    UI fixxed, filters pretty, save does not work

---
+++ app/src/main/java/com/example/moniljhaveri/thebackend/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.moniljhaveri.thebackend;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static com.example.moniljhaveri.thebackend.R.id.image_camera;
+
+
+public class MainActivity extends Activity {
+
+    private static String logtag = "CameraApp8";
+    private static int TAKE_PICTURE = 0; //this is for the camera
+    private Uri imageUri;
+    private static int imageGallery_load_image = 1; //this is for accessing the image gallery
+    private String selectedImagePath;
+
+    private ImageView TakenPhoto; //this is to bring the photo to the gallery
+    TextView textTargetUri;
+
+
+    private OnClickListener onClick = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case (R.id.library_button): //This is what happens when you click on the library button
+                    Intent libraryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(libraryIntent, imageGallery_load_image);
+                    break;
+                case (R.id.camera_button):
+                    takePhoto(view);
+                    break;
+            }
+        }
+    };
+
+    private void takePhoto(View v) { //this is for the camera
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "pictures.jpeg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        startActivityForResult(intent, TAKE_PICTURE);
+
+
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) { //This is for accessing gallery
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Typeface futuraMedium = Typeface.createFromAsset(getApplicationContext().getAssets(), "Futura-Medium.ttf");
+        Button cameraButton = (Button) findViewById(R.id.camera_button);
+        cameraButton.setOnClickListener(onClick);
+        cameraButton.setTypeface(futuraMedium);
+
+        Button libraryButton = (Button) findViewById(R.id.library_button);
+        libraryButton.setTypeface(futuraMedium);
+
+        //get a reference to the image view that the picture will see
+        TakenPhoto = (ImageView) findViewById(image_camera);
+        textTargetUri = (TextView) findViewById(R.id.textView);
+        libraryButton.setOnClickListener(onClick); // this is the listener for the library
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //call parent
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 0:
+                if (resultCode == Activity.RESULT_OK) { //This is the activity for the camera
+                    Uri selectedImage = imageUri;
+                    getContentResolver().notifyChange(selectedImage, null);
+                    TakenPhoto = (ImageView) findViewById(R.id.image_camera);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, selectedImage);
+                        TakenPhoto.setImageBitmap(bitmap);
+                        Toast.makeText(MainActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this, editorpage.class);
+                        startActivity(intent);
+                        break; // don't ask me why it works but it works
+                    }catch (Exception e) {
+                        Toast.makeText(MainActivity.this, "failed to load", Toast.LENGTH_LONG).show();
+                        Log.e(logtag, e.toString());
+                       // break;
+                    }
+                }
+            case 1:
+                if (resultCode == RESULT_OK) { //this is for the library
+                    Uri galleryUri = data.getData();
+                    textTargetUri.setText(galleryUri.toString());
+                    Bitmap bitmap;
+
+                    try {
+                        bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(galleryUri));
+                        TakenPhoto.setImageBitmap(bitmap);
+                        Intent intent = new Intent(MainActivity.this, editorpage.class);
+                        intent.setData(galleryUri);
+                        startActivity(intent);
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+        }
+    }
+}
+
+
+
+//    public String getPath(Uri uri1){ //This gets path of the string image
+//        String res = null;
+//        String[] projection = { MediaStore.Images.Media.DATA };
+//        Cursor cursor = getContentResolver().query(uri1, projection, null, null, null);
+//        if ( cursor.moveToFirst()){;
+//            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+//            cursor.moveToFirst();
+//            res = cursor.getString(column_index);
+//        }
+//        cursor.close();
+//        return res;
+//    }
+//}
+   
---
+++ app/src/main/java/com/example/moniljhaveri/thebackend/editorpage.java
@@ -0,0 +1,401 @@
+package com.example.moniljhaveri.thebackend;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.graphics.Typeface;
+
+
+
+public class editorpage extends Activity {
+    private static String logtag = "editor page";
+    //String[] filterresources;
+    //Spinner filterButton;
+    Bitmap BW;
+    Bitmap editBitmap;
+    ImageView editImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editorpage);
+
+        setContentView(R.layout.activity_editorpage);
+        editImage = (ImageView) findViewById(R.id.image);
+        Uri viewUri = getIntent().getData();
+        try {
+            editBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), viewUri);
+            editImage.setImageBitmap(editBitmap);
+
+            //break; //may need
+        } catch (Exception e){
+            Toast.makeText(editorpage.this, "failed to load", Toast.LENGTH_LONG).show();
+            Log.e(logtag, e.toString());
+        }
+        Button backButton = (Button) findViewById(R.id.back_button); //The back button
+        backButton.setOnClickListener(editorListener);
+        Typeface futuraMedium = Typeface.createFromAsset(getApplicationContext().getAssets(), "Futura-Medium.ttf");
+        backButton.setTypeface(futuraMedium);
+
+        Button Greenify = (Button) findViewById(R.id.green_filter);
+        Greenify.setOnClickListener(editorListener);
+        Greenify.setTypeface(futuraMedium);
+
+
+        Button Filter1= (Button) findViewById(R.id.filter_button);
+        Filter1.setOnClickListener(editorListener);
+        Filter1.setTypeface(futuraMedium);
+
+        Button InvertButton = (Button) findViewById(R.id.invert_filter);
+        InvertButton.setOnClickListener(editorListener);
+        InvertButton.setTypeface(futuraMedium);
+
+        Button Sepia = (Button) findViewById(R.id.sepia_filter);
+        Sepia.setOnClickListener(editorListener);
+        Sepia.setTypeface(futuraMedium);
+
+        Button Bluetrace = (Button) findViewById(R.id.blue_filter);
+        Bluetrace.setOnClickListener(editorListener);
+        Bluetrace.setTypeface(futuraMedium);
+
+        Button Redden = (Button) findViewById(R.id.red_filter);
+        Redden.setOnClickListener(editorListener);
+        Redden.setTypeface(futuraMedium);
+
+
+//        filterButton = (Spinner)findViewById(R.id.Filters); //Filters buttons
+//        filterresources = getResources().getStringArray(R.array.filters); //options of filters
+//        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,filterresources);
+//        filterButton.setAdapter(adapter);
+    }
+
+    private View.OnClickListener editorListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case (R.id.back_button):
+               {
+                   Intent intent = new Intent(editorpage.this, MainActivity.class);
+                   startActivity(intent);
+                   break;
+               }
+               case (R.id.filter_button):
+               {
+                   BW = ChangeGrey(editBitmap,2,1.0,1.0,0.0);
+                   editImage.setImageBitmap(BW);
+                   break;
+               }
+               case (R.id.invert_filter):
+               {
+                   BW = InvertColor(editBitmap);
+                   editImage.setImageBitmap(BW);
+                   break;
+               }
+                case (R.id.sepia_filter):
+                {
+                    BW = createSepiaToningEffect(editBitmap, 50, 1.5, 0.95, 0.12);
+                    editImage.setImageBitmap(BW);
+                    break;
+                }
+               case (R.id.green_filter):
+               {
+                   BW = Greenify(editBitmap);
+                   editImage.setImageBitmap(BW);
+                   break;
+               }
+                case (R.id.blue_filter): {
+                    BW = Bluetrace(editBitmap);
+                    editImage.setImageBitmap(BW);
+                    break;
+                }
+                case (R.id.red_filter): {
+                    BW = Redden(editBitmap);
+                    editImage.setImageBitmap(BW);
+                    break;
+                }
+            }
+        }
+    };
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_editorpage, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static Bitmap ChangeGrey(Bitmap src, int depth, double red, double green, double blue) { //Need to cite this
+        int width = src.getWidth();
+        int height = src.getHeight();
+        Bitmap finalBitmap = Bitmap.createBitmap(width, height, src.getConfig());
+        final double grayScale_Red = 0.3;
+        final double grayScale_Green = 0.59;
+        final double grayScale_Blue = 0.11;
+
+        int channel_aplha, channel_red, channel_green, channel_blue;
+        int pixel;
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                pixel = src.getPixel(x, y);
+                channel_aplha = Color.alpha(pixel);
+                channel_red = Color.red(pixel);
+                channel_green = Color.green(pixel);
+                channel_blue = Color.blue(pixel);
+                channel_blue = channel_green = channel_red = (int)(grayScale_Red * channel_red + grayScale_Green * channel_green + grayScale_Blue * channel_blue);
+                channel_red += (depth * red);
+
+                if(channel_red > 255)
+                {
+                    channel_red = 255;
+                }
+                channel_green += (depth * green);
+                if(channel_green > 255)
+                {
+                    channel_green = 255;
+                }
+                channel_blue += (depth * blue);
+                if(channel_blue > 255)
+                {
+                    channel_blue = 255;
+                }
+                finalBitmap.setPixel(x, y, Color.argb(channel_aplha, channel_red, channel_green, channel_blue));
+            }
+        }
+        return finalBitmap;
+    }
+
+    public static Bitmap InvertColor(Bitmap src) { //android newbie site this, also optimize this
+        // create new bitmap with the same settings as source bitmap
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        // color info
+        int A, R, G, B;
+        int pixelColor;
+        // image size
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        // scan through every pixel
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                // get one pixel
+                pixelColor = src.getPixel(x, y);
+                // saving alpha channel
+                A = Color.alpha(pixelColor);
+                // inverting byte for each R/G/B channel
+                R = 255 - Color.red(pixelColor);
+                G = 255 - Color.green(pixelColor);
+                B = 255 - Color.blue(pixelColor);
+                // set newly-inverted pixel to output image
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+        // return final bitmap
+        return bmOut;
+
+    }
+
+    public static Bitmap Greenify(Bitmap src){
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        int A, R, G, B;
+        int pixelColor;
+        // image size
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        // scan through every pixel
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // get one pixel
+                pixelColor = src.getPixel(x, y);
+                // saving alpha channel
+                A = Color.alpha(pixelColor);
+                // inverting byte for each R/G/B channel
+                if ((Color.red(pixelColor) + Color.green(pixelColor) + Color.blue(pixelColor)) > 70) {
+                    R = Color.red(pixelColor);
+                    G = Color.green(pixelColor) + (255 - Color.green(pixelColor)) / 6;
+                    B = Color.blue(pixelColor);
+                } else if ((Color.red(pixelColor) + Color.green(pixelColor) + Color.blue(pixelColor)) > 120) {
+                    R = Color.red(pixelColor);
+                    G = Color.green(pixelColor) + (255 - Color.green(pixelColor)) / 4;
+                    B = Color.blue(pixelColor);
+                } else {
+                    R = Color.red(pixelColor);
+                    G = Color.green(pixelColor);
+                    B = Color.blue(pixelColor);
+                }
+                // set newly-inverted pixel to output image
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+
+
+        // return final bitmap
+        return bmOut;
+
+
+    }
+
+    public static Bitmap Bluetrace(Bitmap src){
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        int A, R, G, B;
+        int pixelColor;
+        // image size
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        // scan through every pixel
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // get one pixel
+                pixelColor = src.getPixel(x, y);
+                // saving alpha channel
+                A = Color.alpha(pixelColor);
+                // inverting byte for each R/G/B channel
+                if ((Color.red(pixelColor) + Color.green(pixelColor) + Color.blue(pixelColor)) > 70) {
+                    R = Color.red(pixelColor);
+                    G = Color.green(pixelColor);
+                    B = Color.blue(pixelColor) + (255 - Color.green(pixelColor)) / 6;
+                } else if ((Color.red(pixelColor) + Color.green(pixelColor) + Color.blue(pixelColor)) > 120) {
+                    R = Color.red(pixelColor);
+                    G = Color.green(pixelColor);
+                    B = Color.blue(pixelColor) + (255 - Color.green(pixelColor)) / 4;
+                } else {
+                    R = Color.red(pixelColor);
+                    G = Color.green(pixelColor);
+                    B = Color.blue(pixelColor);
+                }
+                // set newly-inverted pixel to output image
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+
+
+        // return final bitmap
+        return bmOut;
+
+
+    }
+
+    public static Bitmap Redden(Bitmap src){
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        int A, R, G, B;
+        int pixelColor;
+        // image size
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        // scan through every pixel
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // get one pixel
+                pixelColor = src.getPixel(x, y);
+                // saving alpha channel
+                A = Color.alpha(pixelColor);
+                // inverting byte for each R/G/B channel
+                if ((Color.red(pixelColor) + Color.green(pixelColor) + Color.blue(pixelColor)) > 70) {
+                    R = Color.red(pixelColor) + (255 - Color.green(pixelColor)) / 6;
+                    G = Color.green(pixelColor);
+                    B = Color.blue(pixelColor);
+                } else if ((Color.red(pixelColor) + Color.green(pixelColor) + Color.blue(pixelColor)) > 120) {
+                    R = Color.red(pixelColor) + (255 - Color.green(pixelColor)) / 4;
+                    G = Color.green(pixelColor);
+                    B = Color.blue(pixelColor);
+                } else {
+                    R = Color.red(pixelColor);
+                    G = Color.green(pixelColor);
+                    B = Color.blue(pixelColor);
+                }
+                // set newly-inverted pixel to output image
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+
+
+        // return final bitmap
+        return bmOut;
+
+
+    }
+
+
+    public static Bitmap createSepiaToningEffect(Bitmap src, int depth, double red, double green, double blue) {
+        // image size
+        int width = src.getWidth();
+        int height = src.getHeight();
+        // create output bitmap
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+        // constant grayscale
+        final double GS_RED = 0.3;
+        final double GS_GREEN = 0.59;
+        final double GS_BLUE = 0.11;
+        // color information
+        int A, R, G, B;
+        int pixel;
+
+        // scan through all pixels
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                // get pixel color
+                pixel = src.getPixel(x, y);
+                // get color on each channel
+                A = Color.alpha(pixel);
+                R = Color.red(pixel);
+                G = Color.green(pixel);
+                B = Color.blue(pixel);
+                // apply grayscale sample
+                B = G = R = (int)(GS_RED * R + GS_GREEN * G + GS_BLUE * B);
+
+                // apply intensity level for sepid-toning on each channel
+                R += (depth * red);
+                if(R > 255) { R = 255; }
+
+                G += (depth * green);
+                if(G > 255) { G = 255; }
+
+                B += (depth * blue);
+                if(B > 255) { B = 255; }
+
+                // set new pixel color to output image
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+        // return final image
+        return bmOut;
+    }
+}
+
+
+
